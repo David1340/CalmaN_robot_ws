98108ed66587a83f30e51d785636d3e37d84dfb0
David1340 <davidnike80@gmail.com>
2025-09-22 11:18:44 -0300

    biblioteca simples do lidar

--- robot_bringup/launch/driver_launch.py
+++ robot_bringup/launch/driver_launch.py
@@ -4,12 +4,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        #Node(package='robot_lidar', executable='lidarX4_node', name='robot_lidar', output='screen'),
-        Node(package='ydlidar',
-            executable='ydlidar_node',
-            name='robot_lidar',
-            parameters=['../../ydlidar_ros2/params/ydlidar.yaml'],
-            output='screen'),
+        Node(package='robot_lidar', executable='lidarX4_node', name='robot_lidar', output='screen'),
+        #Node(package='ydlidar',
+        #    executable='ydlidar_node',
+        #    name='robot_lidar',
+        #    parameters=['../../ydlidar_ros2/params/ydlidar.yaml'],
+        #    output='screen'),
         Node(package='robot_camera',
             executable='camera_node', 
             name='robot_camera', 
