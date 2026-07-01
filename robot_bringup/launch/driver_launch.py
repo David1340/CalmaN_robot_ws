3103ec05dcf83a01f12d5391a516819ef8ef2f32
david1340 <davidnike80@gmail.com>
2026-07-01 14:37:23 +0100

    ajustes para o robot da ul

--- robot_bringup/launch/driver_launch.py
+++ robot_bringup/launch/driver_launch.py
@@ -18,18 +18,13 @@ def generate_launch_description():
         'rsp.launch.py'
     )
 
-    lidar_launch = os.path.join(
-        get_package_share_directory('ydlidar_ros2_driver'),
+    rplidar_launch = os.path.join(
+        get_package_share_directory('rplidar_ros'),
         'launch',
-        'ydlidar_launch.py'
+        'rplidar_a2m8_launch.py'
     )
-
         
     return LaunchDescription([
-        #Node(package='robot_lidar', 
-        #    executable='lidarX4_node',
-        #    name='robot_lidar',
-        #    output='screen'),
         Node(package='robot_camera',
             executable='camera_node', 
             name='robot_camera', 
@@ -38,17 +33,13 @@ def generate_launch_description():
             executable='stm32_bridge',
             name='stm32_bridge_node',
             output='screen'),
-        #Node(package='robot_bringup', 
-        #    executable='encoder_node',
-        #    name='encoder_node_odom',
-        #    output='screen'),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(description_launch)
         ),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(lidar_launch)
+            PythonLaunchDescriptionSource(rplidar_launch)
         ),
 
     ])
--- stm32_bridge/stm32_bridge/bridge_node.py
+++ stm32_bridge/stm32_bridge/bridge_node.py
@@ -7,11 +7,6 @@ from std_msgs.msg import Float32MultiArray
 import serial
 import numpy as np
 
-def bitset(valor, pos, val=1):
-    if val:
-        return valor | (1 << (pos - 1))
-    else:
-        return valor & ~(1 << (pos - 1))
 
 def inverter_uL(x):
     return np.sign(x)*(np.abs(x) - (-0.414))/1.459
@@ -37,8 +32,8 @@ class STM32Bridge(Node):
         #parâmetros configuráveis
         self.declare_parameter('serial_port', '/dev/ttyACM0')
         self.declare_parameter('baud_rate', 115200)
-        self.declare_parameter('wheels_radius', 0.03)  # em metros
-        self.declare_parameter('wheels_distance', 0.173) # distância entre as rodas em metros
+        self.declare_parameter('wheels_radius', 0.033)  # em metros
+        self.declare_parameter('wheels_distance', 0.159) # distância entre as rodas em metros
 
         #carregar parâmetros
         self.port = self.get_parameter('serial_port').value
@@ -65,15 +60,8 @@ class STM32Bridge(Node):
             10 
             )
         
-        self.imu_pub = self.create_publisher(
-        Imu,
-        '/robot/imu',  
-        10 
-        )
 
         self.serial_time = 2e-3 # 500 Hz (2 ms)
-        #self.publish_time = 4e-3 # 250 Hz (4ms)
-        #self.cnt = 0
 
         self.timer = self.create_timer(self.serial_time, self.read_serial)  # 500 Hz (2 ms)
 
@@ -102,45 +90,15 @@ class STM32Bridge(Node):
             msg = self._montar_mensagem(self.v_r, self.v_l)
             self.serial.write(msg)
             try:
-                data = self.serial.read(20)  # Ler 20 bytes
+                data = self.serial.read(4)  # Ler 4 bytes
                 values = np.frombuffer(data, dtype=np.int16).astype(np.float32)
-
 
                 # publicando encoder
                 encoder_msg = Float32MultiArray()
                 tick2rad = (2*np.pi/(4*224.4))
-                phiE = values[2]*tick2rad/(10e-3)#rad/s
-                phiD = values[3]*tick2rad/(10e-3)# #rad/s
-                encoder_msg.data = [(values[0] + 32000)*tick2rad,(values[1] + 32000)*tick2rad,
-                                    phiE,phiD] # encoder1 e encoder2
+                encoder_msg.data = [values[0]*tick2rad,values[1]*tick2rad] # encoder1 e encoder2
                 self.encoder_publisher.publish(encoder_msg)
 
-                # publicando IMU
-                imu_msg = Imu()
-                imu_msg.linear_acceleration.x = values[4]*(9.81/16384.0)
-                imu_msg.linear_acceleration.y = values[5]*(9.81/16384.0)
-                imu_msg.linear_acceleration.z = values[6]*(9.81/16384.0)
-                imu_msg.linear_acceleration_covariance = [
-                    7.120e-04, -1.420e-04, -8.800e-05, 
-                    -1.420e-04, 5.170e-04, 9.700e-05, 
-                    -8.800e-05, 9.700e-05, 2.698e-03]
-                
-                imu_msg.angular_velocity.x = values[7]*np.pi/(180.0*131.0)
-                imu_msg.angular_velocity.y = values[8]*np.pi/(180.0*131.0)
-                imu_msg.angular_velocity.z = values[9]*np.pi/(180.0*131.0)
-                imu_msg.angular_velocity_covariance = [
-                    6.15e-06, -1.06e-06, 7.00e-08,
-                    -1.06e-06,  9.08e-06, -3.20e-07,
-                    7.00e-08, -3.20e-07,  5.58e-06]
-                self.imu_pub.publish(imu_msg)
-
-                
-                #if self.cnt > (self.publish_time/self.serial_time):
-                    # publicando encoder
-                #    self.encoder_publisher.publish(encoder_msg)
-                    # publicando IMU
-                #    self.imu_pub.publish(imu_msg)
-                #    self.cnt = 0
 
             except Exception as e:
                 self.get_logger().error(f'Error reading from serial: {e}')
@@ -151,30 +109,26 @@ class STM32Bridge(Node):
         return v_r, v_l
     
     def _montar_mensagem(self, v_r, v_l):
-        Vmax = 25.2 #rad/s velocidade máxima de cada roda
+        #Vmax = 25.2 #rad/s velocidade máxima de cada roda
+        Vmax = 250 #bit
         uL = v_l/Vmax #m/s ->  -1 a 1
         uR = v_r/Vmax #m/s ->  -1 a 1
 
-        uL = satura(inverter_uL(uL))
-        uR = satura(inverter_uR(uR))
+        #uL = satura(inverter_uL(uL))
+        #uR = satura(inverter_uR(uR))
 
-        msg = [254, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        logica = 0
+        msg = [254, 0, 0, 0, 0]
+        msg[1] = int(round(250 * abs(uR)))
+        msg[3] = int(round(250 * abs(uL)))
         if uR >= 0:
-            msg[8] = int(round(250 * abs(uR)))
-            logica = bitset(logica, 2)
+            msg[2] = 0
         else:
-            msg[8] = int(round(250 * abs(uR)))
-            logica = bitset(logica, 3)
+            msg[2] = 1
 
         if uL >= 0:
-            msg[9] = int(round(250 * abs(uL)))
-            logica = bitset(logica, 4)
+            msg[4] = 0
         else:
-            msg[9] = int(round(250 * abs(uL)))
-            logica = bitset(logica, 5)
-
-        msg[7] = logica
+            msg[4] = 1
 
         return bytes(msg)
 
